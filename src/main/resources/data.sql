d7c565c26584fcb4f750edf2cd41ea1413956bea
Sagar Dodia <sdodia@walmartlabs.com>
2018-01-23 14:35:47 -0800

    Add JPA to connect in memory H2 DB

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into user values(10001, sysdate(), 'Sagar');
+insert into user values(10002, sysdate(), 'Hiral');
+insert into user values(10003, sysdate(), 'Suhani');
+insert into post values(11001, 'Sagar First Post', 10001);
+insert into post values(11002, 'Hiral First Post', 10001);
+insert into post values(11003, 'Suhani First Post', 10001);
